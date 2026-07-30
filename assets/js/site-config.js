ad0a01402a0ed35def2c747e3868ab5a8ed2d4ec
rishi-krishna <rishi.thodupunuri@gmail.com>
2026-07-30 14:14:16 -0400

    chore: establish static site configuration

---
+++ assets/js/site-config.js
@@ -0,0 +1,18 @@
+export const clinic = Object.freeze({
+  practitioner: "Arunima Mustyala",
+  clinicName: "Arunima Mustyala Clinic",
+  credentials: "BHMS | PG Hom (London) | Fellowship (Germany)",
+  registration: "PR-3641/H/2025",
+  board: "Telangana Board",
+  experience: "15+ Years Experience",
+  patientCount: "5,000+",
+  rating: "4.9",
+  reviewCount: "600+",
+  phoneDisplay: "+91 90665 62562",
+  phoneHref: "tel:+919066562562",
+  whatsappHref: "https://wa.me/919066562562",
+  email: "drnehasclinic@gmail.com",
+  address:
+    "Shop No. 401, Ganesh Plaza, JNTU Hitech City Road, KPHB, Kukatpally, Hyderabad - 500085",
+  assessmentPrice: "₹999",
+});
